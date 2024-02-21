582c57911ece3607d91067e74511af5c54989325
rick <jing.xin@lista.org>
2024-02-21 15:57:05 +0800

    fea: change config

---
+++ cmd/bot/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/helio-money/auctionbot/internal/jobs"
+	"github.com/helio-money/auctionbot/pkg/config"
+	"github.com/namsral/flag"
+	"github.com/sirupsen/logrus"
+	"math/big"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+var configFile = flag.String("config", "./config/config.yaml", "config file path")
+
+func main() {
+	flag.Parse()
+
+	cfg, err := config.LoadConfig(*configFile)
+	if err != nil {
+		logrus.Errorf("config.LoadConfig err: %v", err)
+	}
+
+	if !Run(cfg) {
+		os.Exit(1)
+	}
+}
+
+const (
+	commandBuyFlashAuction = "buy_flash_auction"
+	commandBuyAction       = "buy_auction"
+	commandResetAction     = "reset_auction"
+	commandStartAction     = "start_auction"
+)
+
+func Run(cfg *config.Config) bool {
+	args := cfg.Commands
+	if len(args) == 0 {
+		panic(fmt.Sprintf("please set bot mode %v", []string{
+			commandBuyAction,
+			commandResetAction,
+			commandStartAction,
+			commandBuyFlashAuction,
+		}))
+	}
+
+	resource, err := config.LoadEnvironmentResource(cfg)
+	if err != nil {
+		logrus.Fatalf("config.LoadEnvironmentResource err: %v", err)
+	}
+
+	interaction := common.HexToAddress(cfg.Contract.Interaction)
+	hay := common.HexToAddress(cfg.Contract.Hay)
+	flushBuy := common.HexToAddress(cfg.Contract.FlushBuy)
+	collaterals := make([]common.Address, 0, len(cfg.Contract.Collaterals))
+	for _, c := range cfg.Contract.Collaterals {
+		collaterals = append(collaterals, common.HexToAddress(c))
+	}
+
+	jj := make([]jobs.Job, len(args)*len(collaterals))
+	for ind, arg := range args {
+		for _, collateral := range collaterals {
+			switch arg {
+			case commandResetAction:
+				jj[ind] = jobs.NewResetAuctionJob(
+					context.Background(),
+					resource.Log,
+					resource.Wallet,
+					resource.HttpNodeClient,
+					interaction,
+					collateral,
+					true,
+				)
+			case commandBuyAction:
+				jj[ind] = jobs.NewBuyAuctionJob(
+					context.Background(),
+					resource.Log,
+					resource.Wallet,
+					resource.HttpNodeClient,
+					interaction,
+					collateral,
+					hay,
+					big.NewInt(cfg.Settings.MaxPricePercentage),
+					true,
+				)
+			case commandStartAction:
+				jj[ind] = jobs.NewStartAuctionJob(
+					context.Background(),
+					resource.Log,
+					resource.Wallet,
+					resource.AnalyticsClient,
+					resource.HttpNodeClient,
+					interaction,
+					collateral,
+					true,
+				)
+			case commandBuyFlashAuction:
+				// is address zero checking
+				if bytes.Compare(flushBuy.Bytes(), common.Address{}.Bytes()) == 0 {
+					panic(fmt.Sprintf("FLASHBUY contract must be set for %s mode", commandBuyFlashAuction))
+				}
+
+				jj[ind] = jobs.NewBuyFlashAuctionJob(
+					context.Background(),
+					resource.Log,
+					resource.Wallet,
+					resource.HttpNodeClient,
+					interaction,
+					collateral,
+					hay,
+					flushBuy,
+					big.NewInt(cfg.Settings.MaxPricePercentage),
+					true,
+				)
+			default:
+				panic(fmt.Sprintf("such command %s is not exists", arg))
+			}
+		}
+
+	}
+
+	ctx, cancel := ctxWithSig()
+	defer cancel()
+	defer func() {
+		if err := recover(); err != nil {
+			resource.Log.Error(err)
+			cancel()
+		}
+	}()
+
+	wg := &sync.WaitGroup{}
+	for _, j := range jj {
+		wg.Add(1)
+		j.Run(ctx, wg)
+	}
+
+	wg.Wait()
+	return true
+}
+
+func ctxWithSig() (context.Context, func()) {
+	ctx, cancel := context.WithCancel(context.Background())
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGINT)
+
+	go func() {
+		select {
+		case <-ch:
+			cancel()
+		}
+	}()
+
+	return ctx, cancel
+}
---
+++ pkg/config/resource.go
@@ -0,0 +1,94 @@
+package config
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	analyticsv1 "github.com/helio-money/auctionbot/internal/analytics/v1"
+	"github.com/helio-money/auctionbot/internal/wallet"
+	"github.com/sirupsen/logrus"
+	"net/url"
+)
+
+type Resource struct {
+	Log             *logrus.Logger
+	HttpNodeClient  *ethclient.Client
+	WsNodeClient    *ethclient.Client
+	Wallet          wallet.Walleter
+	AnalyticsClient *analyticsv1.Client
+}
+
+func LoadEnvironmentResource(config *Config) (*Resource, error) {
+	var (
+		resource Resource
+		err      error
+	)
+	resource.Log, err = initLogger(config.Log.Level)
+	if err != nil {
+		return nil, fmt.Errorf("initLogger err: %w", err)
+	}
+
+	analyticsUrl, err := url.Parse(config.Analytics.Url)
+	if err != nil {
+		return nil, fmt.Errorf("url.Parse err: %w", err)
+	}
+	resource.AnalyticsClient = analyticsv1.NewClient(analyticsUrl)
+
+	resource.HttpNodeClient, err = initHttpNodeClient(config.RpcNode.Http)
+	if err != nil {
+		return nil, fmt.Errorf("initHttpNodeClient err: %w", err)
+
+	}
+	resource.WsNodeClient, err = initWsNodeClient(config.RpcNode.Ws)
+	if err != nil {
+		return nil, fmt.Errorf("initWsNodeClient err: %w", err)
+	}
+
+	resource.Wallet, err = initWallet(config.Wallet.PrivateKey, resource.HttpNodeClient)
+	if err != nil {
+		return nil, fmt.Errorf("initWallet err: %w", err)
+	}
+
+	return &resource, nil
+}
+
+func initLogger(level string) (*logrus.Logger, error) {
+	l, err := logrus.ParseLevel(level)
+	if err != nil {
+		return nil, fmt.Errorf("logrus.ParseLevel err: %w", err)
+	}
+
+	log := logrus.New()
+	log.Level = l
+	return log, nil
+}
+
+func initWallet(privateKey string, httpClient *ethclient.Client) (wallet.Walleter, error) {
+	var err error
+	prvk, err := crypto.HexToECDSA(privateKey)
+	if err != nil {
+		return nil, fmt.Errorf("crypto.HexToECDSA err: %w", err)
+	}
+
+	w := wallet.NewWallet(httpClient, prvk)
+
+	return w, nil
+}
+
+func initHttpNodeClient(httpURL string) (*ethclient.Client, error) {
+	httpCli, err := ethclient.Dial(httpURL)
+	if err != nil {
+		return nil, fmt.Errorf("ethclient.Dial err: %w", err)
+	}
+
+	return httpCli, nil
+}
+
+func initWsNodeClient(wsURL string) (*ethclient.Client, error) {
+	wsCli, err := ethclient.Dial(wsURL)
+	if err != nil {
+		return nil, fmt.Errorf("ethclient.Dial err: %w", err)
+	}
+
+	return wsCli, nil
+}
